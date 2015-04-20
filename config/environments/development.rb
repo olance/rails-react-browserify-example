8f91767ba74ff77aa342490e9e3dfb1759175cc9
Olivier Lance <olivier@digitalcuisine.fr>
2015-04-20 20:20:45 +0200

    Basic configuration of react-rails See [detailed article](https://medium.com/@olance/isomorphic-reactjs-app-with-ruby-on-rails-part-1-server-side-rendering-8438bbb1ea1c)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Server development version of React
+  config.react.variant = :development
 end
