e39fd02804e67aa7fe77dd4001beed92178810e1
Olivier Lance <olivier@digitalcuisine.fr>
2015-04-20 20:35:27 +0200

    Using reactify for Browserify

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module RailsReactBrowserifyExample
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Tell browserify-rails how to treat .js.jsx files
+    config.browserify_rails.commandline_options = "-t reactify --extension=\".js.jsx\""
   end
 end
